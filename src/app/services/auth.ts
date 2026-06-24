c81b8b95256279917b4395820edaf19c04790e6c
Megyeri Márk Máté <megyko1234@gmail.com>
2026-06-24 18:59:50 +0200

    belépési component létrehozbva

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,8 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
-  
+  private apiUrl = 'http://127.0.0.1:800/api';
+
+  constructor(private http: HttpClient) {}
+
+  login(credentials: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, credentials);
+  }
+
+  logout() {
+    localStorage.removeItem('auth_token');
+  }
+
+  getToken() {
+    return localStorage.getItem('auth_token');
+  }
 }
